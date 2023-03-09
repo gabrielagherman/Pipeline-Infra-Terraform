af053b634d1e5abe9a9561f1d3f2f1cea5caa7f3
gabrielagherman <gabi@DESKTOP-DMM6IVT.localdomain>
2023-03-09 21:23:11 +0200

    create an instance-again

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "myec2test" {
   ami           = "ami-00874d747dde814fa"  
   instance_type = "t2.micro"
   tags = {
-    "Name" : "createdByJenkins"
+    "Name" : "createdBy_Jenkins"
   }
 }
